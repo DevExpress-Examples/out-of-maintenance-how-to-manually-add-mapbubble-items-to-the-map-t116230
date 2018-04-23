b8c34bb89c73aceed52e58f12c6d6e3d98dcec8b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MapBubbleItem/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+
+namespace MapBubbleItem {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a layer to show vector items.
+            VectorItemsLayer itemsLayer = new VectorItemsLayer();
+            mapControl1.Layers.Add(itemsLayer);
+
+            // Create a storage to provide data for the vector layer.
+            MapItemStorage storage = new MapItemStorage();
+            itemsLayer.Data = storage;
+
+            // Create an array of Bubble items with different values, sizes and locations.
+            MapBubble bubble1 = new MapBubble() { Argument = "A", Value = 200, Location = new GeoPoint(-45, -60), Size = 20 };
+            MapBubble bubble2 = new MapBubble() { Argument = "B", Value = 400, Location = new GeoPoint(-45, 0), Size = 40 };
+            MapBubble bubble3 = new MapBubble() { Argument = "C", Value = 800, Location = new GeoPoint(-45, 60), Size = 80 };
+            storage.Items.AddRange(new MapItem[] { bubble1, bubble2, bubble3 });
+
+            // Provide color indexes to bubbles as attributes.
+            int i = 0;
+            foreach (MapItem bubble in storage.Items) {
+                bubble.Attributes.Add(new MapItemAttribute() { Name = "Color", Value = i });
+                i++;
+            }
+
+            // Create a colorizer to provide colors for bubble items.
+            ColorIndexColorizer colorizer = new ColorIndexColorizer();
+            itemsLayer.Colorizer = colorizer;
+
+            // Add colors to the colorizer.
+            colorizer.ColorItems.Add(new ColorizerColorTextItem() { Color = Color.Coral, Text = "Category A" });
+            colorizer.ColorItems.Add(new ColorizerColorTextItem() { Color = Color.Orange, Text = "Category B" });
+            colorizer.ColorItems.Add(new ColorizerColorTextItem() { Color = Color.LightBlue, Text = "Category C" });
+
+            // Load color indexes from bubbles via the 'Color' attribute
+            colorizer.ColorIndexProvider = new ShapeAttributeColorIndexProvider() { AttributeName = "Color" };
+
+            
+            // Show a color legend.
+            mapControl1.Legends.Add(new ColorListLegend() { Layer = itemsLayer });
+        }
+
+    }
+
+}
---
+++ VB/MapBubbleItem/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.XtraMap
+
+Namespace MapBubbleItem
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' Create a layer to show vector items.
+			Dim itemsLayer As New VectorItemsLayer()
+			mapControl1.Layers.Add(itemsLayer)
+
+			' Create a storage to provide data for the vector layer.
+			Dim storage As New MapItemStorage()
+			itemsLayer.Data = storage
+
+			' Create an array of Bubble items with different values, sizes and locations.
+			Dim bubble1 As New MapBubble() With {.Argument = "A", .Value = 200, .Location = New GeoPoint(-45, -60), .Size = 20}
+			Dim bubble2 As New MapBubble() With {.Argument = "B", .Value = 400, .Location = New GeoPoint(-45, 0), .Size = 40}
+			Dim bubble3 As New MapBubble() With {.Argument = "C", .Value = 800, .Location = New GeoPoint(-45, 60), .Size = 80}
+			storage.Items.AddRange(New MapItem() { bubble1, bubble2, bubble3 })
+
+			' Provide color indexes to bubbles as attributes.
+			Dim i As Integer = 0
+			For Each bubble As MapItem In storage.Items
+				bubble.Attributes.Add(New MapItemAttribute() With {.Name = "Color", .Value = i})
+				i += 1
+			Next bubble
+
+			' Create a colorizer to provide colors for bubble items.
+			Dim colorizer As New ColorIndexColorizer()
+			itemsLayer.Colorizer = colorizer
+
+			' Add colors to the colorizer.
+			colorizer.ColorItems.Add(New ColorizerColorTextItem() With {.Color = Color.Coral, .Text = "Category A"})
+			colorizer.ColorItems.Add(New ColorizerColorTextItem() With {.Color = Color.Orange, .Text = "Category B"})
+			colorizer.ColorItems.Add(New ColorizerColorTextItem() With {.Color = Color.LightBlue, .Text = "Category C"})
+
+			' Load color indexes from bubbles via the 'Color' attribute
+			colorizer.ColorIndexProvider = New ShapeAttributeColorIndexProvider() With {.AttributeName = "Color"}
+
+
+			' Show a color legend.
+			mapControl1.Legends.Add(New ColorListLegend() With {.Layer = itemsLayer})
+		End Sub
+
+	End Class
+
+End Namespace
